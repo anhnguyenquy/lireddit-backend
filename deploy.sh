7b15d976ee15218e0a70fd7dd58fec70c3ea52f9
anhnguyenquy <anhnguyenquy2407@gmail.com>
2022-07-11 14:44:19 +0700

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-read -p "What should the version be? " VERSION
-docker build -t anhnguyenquy/lireddit:$VERSION .
-docker push anhnguyenquy/lireddit:$VERSION
-ssh admin@54.179.207.146 -i /home/anq/.ssh/Admin.pem "sudo docker pull anhnguyenquy/lireddit:$VERSION && sudo dokku git:from-image backend anhnguyenquy/lireddit:$VERSION"
+docker build -t anhnguyenquy/lireddit:latest .
+docker push anhnguyenquy/lireddit:latest
+ssh admin@54.179.207.146 -i /home/anq/.ssh/Admin.pem "sudo docker pull anhnguyenquy/lireddit:latest && sudo dokku git:from-image backend anhnguyenquy/lireddit:latest"
